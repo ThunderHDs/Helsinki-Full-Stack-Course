182f6838b6cefa2aad0dd3237745da7b691722ed
Javier da Silva <jhernandezdasilva@gmail.com>
2025-10-25 14:23:55 +0100

    part 2.9 the phonebook done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,35 +2,44 @@ import { useState } from 'react';
 import Person from './components/person.jsx';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: 12345, key: 1}
+  //Main states
+  const [persons, setPersons] = useState([ //Person states for storing the data
+    { name: 'Arto Hellas', number: '040-123456', key: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', key: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', key: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', key: 4 }
   ]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
+  const [newName, setNewName] = useState(''); //State to get the new contact name
+  const [newNumber, setNewNumber] = useState(''); //State to get the new contact number
+  const [filter, setFilter] = useState(''); //State to get the filter name
+  
+  //Creating the new array of persons based on the filter applied, if the input is empty, it shows all
+  const newPersons = persons.filter(person=>person.name.includes(filter));
 
+  //Function to create new contact
   const addNewName = (event) => {
     event.preventDefault();
-    //console.log('log',newName);
+    //Validation of repeating contact
     const found = persons.find((person)=>person.name===newName);
 
     if(!found){
+      //if contact is new, we create a new contact object
       const personObject = {
         name: newName,
         number: newNumber,
         key: String(persons.length + 1)
       }
-
+      //then we save the new contact and reset the name and number states
       setPersons(persons.concat(personObject));
       setNewName('');
       setNewNumber('');
     } else {
       alert(`${newName} is already added to phonebook`);
     }
-    console.log('persons', persons);
   };
 
+  //Change handlers
   const handleNewName = (event) => {
-    //console.log(event.target.value);
     setNewName(event.target.value);
   };
 
@@ -38,10 +47,15 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
+      filter shown with <input value={filter} onChange={handleFilterChange}/>
+      <div>debug: {filter}</div>
       <form onSubmit={addNewName}>
         <div>
           name: <input 
@@ -61,7 +75,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         <ul>
-          {persons.map(person=> 
+          {newPersons.map(person=> 
             <Person key={person.key} name={person.name} number={person.number}/>
           )}
         </ul>
